070164ccc002d60d7fe9d94ddfa382df3f5b0939
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-15 20:11:47 -0600

    add create, update, destroy endpoints with strong params for volunteers
    add resources

--- app/controllers/api/v1/volunteers_controller.rb
+++ app/controllers/api/v1/volunteers_controller.rb
@@ -16,4 +16,43 @@ class Api::V1::VolunteersController < ApplicationController
             render json: { error: e.message }, status: :not_found
         end
     end
+
+    def create
+        @volunteer = Volunteer.new(volunteer_params)
+
+        if @volunteer.save
+            render json: @volunteer.to_json, status: :created
+        else
+            render json: { errors: @volunteer.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @volunteer = Volunteer.find(params[:id])
+
+        if @volunteer.update(volunteer_params)
+            render json: @volunteer.to_json
+        else
+            render json: { errors: @volunteer.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @volunteer = Volunteer.find(params[:id])
+        if @volunteer.destroy
+            head :no_content
+        else
+            render json: { errors: @volunteer.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def volunteer_params
+        params.require(:volunteer).permit(:name, :email, :phone)
+    end
+
+    def record_not_found
+        render json: { error: 'Record not found' }, status: :not_found
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :volunteers, only: [:index, :show]
+      resources :volunteers, only: [:index, :show, :update, :create, :destroy]
       resources :campaigns, only: [:index, :show, :update, :create, :destroy]
     end
   end

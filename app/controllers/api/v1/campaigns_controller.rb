c5dd433aa9dedfbf55f3708e425bf1a95cc0f4eb
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-15 20:08:22 -0600

    add update, create, destroy endpoints with strong params for campaigns
    add resources

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::CampaignsController < ApplicationController 
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
     def index
         begin
             @campaigns = Campaign.all
@@ -16,4 +18,44 @@ class Api::V1::CampaignsController < ApplicationController
             render json: { error: e.message }, status: :not_found
         end
     end
+
+    def create
+    @campaign = Campaign.new(campaign_params)
+
+    if @campaign.save
+      render json: @campaign.to_json, status: :created
+    else
+      render json: { errors: @campaign.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @campaign = Campaign.find(params[:id])
+
+    if @campaign.update(campaign_params)
+      render json: @campaign.to_json
+    else
+      render json: { errors: @campaign.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @campaign = Campaign.find(params[:id])
+    
+    if @campaign.destroy
+      head :no_content
+    else
+      render json: { errors: @campaign.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def campaign_params
+    params.require(:campaign).permit(:name, :description, :start_date, :end_date)
+  end
+
+  def record_not_found
+    render json: { error: 'Record not found' }, status: :not_found
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :volunteers, only: [:index, :show]
-      resources :campaigns, only: [:index, :show]
+      resources :campaigns, only: [:index, :show, :update, :create, :destroy]
     end
   end
 end

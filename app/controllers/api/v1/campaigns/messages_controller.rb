2b41deb32c6b8e37b4943927040e9a73940e67ba
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-16 09:11:52 -0600

    Add messages model / migration / spec / controller / nested route resources
    Add endpoint for sending message to volunteers

---
+++ app/controllers/api/v1/campaigns/messages_controller.rb
@@ -0,0 +1,74 @@
+class Api::V1::Campaigns::MessagesController < ApplicationController
+  before_action :set_campaign
+  before_action :set_message, only: [:show, :update, :destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActionController::ParameterMissing, with: :render_bad_request_response
+
+  def index
+    messages = @campaign.messages
+    render json: messages
+  end
+
+  def show
+    render json: @message
+  end
+
+  def create
+    message = @campaign.messages.create!(message_params)
+    render json: message, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+  def update
+    @message.update!(message_params)
+    render json: @message
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+  def destroy
+    @message.destroy
+    head :no_content
+  end
+
+  def send_to_campaign_volunteers
+		# call campaign model method that will send message to each connected CampaignVolunteer
+	
+    head :no_content
+    rescue StandardError => e
+    	render json: { error: e.message }, status: :internal_server_error
+	end
+
+  private
+
+  def set_campaign
+    @campaign = Campaign.find_by(id: params[:campaign_id])
+    render json: { error: "Campaign not found" }, status: :not_found unless @campaign
+  end
+
+  def set_message
+    @message = @campaign.messages.find_by(id: params[:id])
+    render json: { error: "Message not found" }, status: :not_found unless @message
+  end
+
+  def message_params
+    params.require(:message).permit(:text)
+  rescue ActionController::ParameterMissing
+    render json: { error: "Missing message parameters" }, status: :bad_request
+  end
+
+  def render_not_found_response
+    render json: { error: "Campaign not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(exception)
+    render json: { error: exception.message }, status: :unprocessable_entity
+  end
+
+  def render_bad_request_response
+    render json: { error: "Bad request" }, status: :bad_request
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :volunteers, only: [:index, :show, :update, :create, :destroy]
-      resources :campaigns, only: [:index, :show, :update, :create, :destroy]
+      resources :volunteers
+      resources :campaigns do
+        resources :messages do
+          # post :send_to_campaign_volunteers, on: :collection
+          match 'send_to_campaign_volunteers', via: %i[post], as: :send_to_campaign_volunteers, on: :collection
+        end
+      end
     end
   end
 end

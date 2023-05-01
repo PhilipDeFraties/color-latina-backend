56f6545715614d532ccd28a9fde929e1dc4e0c9d
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-30 22:57:28 -0600

    add spec file

--- spec/models/volunteer_spec.rb
+++ spec/models/volunteer_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Volunteer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#send_message' do
+    let(:volunteer) { create(:volunteer) }
+    let(:campaign) { create(:campaign) }
+    let(:message) { create(:message, campaign: campaign) }
+
+    it 'sends message via TwilioService' do
+      client = double(Twilio::REST::Client)
+      messages = double('Messages')
+      allow(client).to receive(:messages).and_return(messages)
+      allow(Twilio::REST::Client).to receive(:new).and_return(client)
+
+      expect(messages).to receive(:create).with(
+        from: ENV['TWILIO_PHONE_NUMBER'],
+        to: volunteer.phone,
+        body: message.text
+      )
+
+      volunteer.send_message(message.id)
+    end
+  end
 end

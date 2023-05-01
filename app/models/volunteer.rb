1de426cd45f0c46ede95cc124859ee6a7f895b23
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-30 22:55:13 -0600

    update model validations

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,11 +1,13 @@
-require_relative '../services/twilio_service'
-
 class Volunteer < ApplicationRecord
-	has_many :campaign_volunteers
-	has_many :campaigns, through: :campaign_volunteers
+  has_many :campaign_volunteers, dependent: :destroy
+  has_many :campaigns, through: :campaign_volunteers
+  has_many :messages, through: :campaigns
 
-	def send_message(message_id)
-		message = Message.find(message_id)
-		TwilioService.new.send_sms(phone, message.text)
-	end
+  validates :name, presence: true
+  validates :phone, presence: true
+
+  def send_message(message_id)
+    message = messages.find(message_id)
+    TwilioService.send_sms(phone, message.text) if message.present?
+  end
 end

c9c1ac2408570b54bced4ee3c188812c17731392
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-30 22:54:46 -0600

    update model validations

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,11 +1,13 @@
 class Campaign < ApplicationRecord
-	has_many :campaign_volunteers
-	has_many :volunteers, through: :campaign_volunteers
-	has_many :messages
+  has_many :campaign_volunteers, dependent: :destroy
+  has_many :volunteers, through: :campaign_volunteers
+  has_many :messages, dependent: :destroy
 
-	def send_message_to_volunteers(message_id)
-		volunteers.each do |volunteer|
-			volunteer.send_message(message_id)
-		end
-	end
+  validates :name, presence: true
+
+  def send_message_to_volunteers(message_id)
+    volunteers.each do |volunteer|
+      volunteer.send_message(message_id)
+    end
+  end
 end

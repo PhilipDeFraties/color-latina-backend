48c76da57517bd6da81fc527f52736b2799ad7af
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-30 22:57:19 -0600

    add spec file

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Campaign, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:campaign_volunteers).dependent(:destroy) }
+    it { should have_many(:volunteers).through(:campaign_volunteers) }
+    it { should have_many(:messages).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe '#send_message_to_volunteers' do
+    let(:campaign) { create(:campaign) }
+    let(:message) { create(:message, campaign: campaign) }
+    let(:volunteer1) { instance_double(Volunteer) }
+    let(:volunteer2) { instance_double(Volunteer) }
+
+    before do
+      allow(volunteer1).to receive(:send_message)
+      allow(volunteer2).to receive(:send_message)
+      allow(campaign).to receive(:volunteers).and_return([volunteer1, volunteer2])
+    end
+
+    it 'sends message to all volunteers associated with the campaign' do
+      campaign.send_message_to_volunteers(message.id)
+      expect(volunteer1).to have_received(:send_message).with(message.id)
+      expect(volunteer2).to have_received(:send_message).with(message.id)
+    end
+  end
 end
